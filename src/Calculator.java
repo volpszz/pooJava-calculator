36908218e8e8459730ed30e90708d1d69e466901
volpsz <76572284+volpszz@users.noreply.github.com>
2026-03-12 20:58:59 -0300

    Refactor Calculator class to improve structure

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,42 +1,52 @@
 import java.util.Scanner;
 import calculus.Calculus;
+
+
 public class Calculator {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        runCalculator(sc);
+    }
+    
+    private static void runCalculator (Scanner sc){
         char again;
         double a;
         double b;
         char type;
-        System.out.println("Welcome to Calculator!");
 
-        do {
-            System.out.println("What is the first number?");
-            a = sc.nextDouble();
-            System.out.println("What is the second number?");
-            b = sc.nextDouble();
-            System.out.println("What operation do you want? (+ | - | * | /)");
-            type = sc.next().charAt(0);
+            System.out.println("Welcome to Calculator!");
 
-            switch (type) {
-                case '+':
-                    Calculus.showResult(Calculus.add(a,b));
-                    break;
-                case '-':
-                    Calculus.showResult(Calculus.subtract(a,b));
-                    break;
-                case '*':
-                    Calculus.showResult(Calculus.multiply(a,b));
-                    break;
-                case '/':
-                    Calculus.showResult(Calculus.divide(a,b));
-                    break;
+            do {
+                System.out.println("What is the first number?");
+                a = sc.nextDouble();
 
-            }
-            System.out.println("Do you want to continue? (s/n)");
-            again = sc.next().toLowerCase().charAt(0);
-        } while (again == 's');
-        System.out.println("Ending Calculator!");
-        sc.close();
+                System.out.println("What is the second number?");
+                b = sc.nextDouble();
+
+                System.out.println("What operation do you want? (+ | - | * | /)");
+                type = sc.next().charAt(0);
+
+                processOperation(a,b,type);
+
+                System.out.println("Do you want to continue? (s/n)");
+                again = sc.next().toLowerCase().charAt(0);
+
+            } while (again == 's');
+
+            System.out.println("Ending Calculator!");
+            sc.close();
+        }
+
+
+private static void processOperation(double a,double b,char type){
+    switch(type){
+        case '+': Calculus.showResult(Calculus.add(a,b));
+        case '-': Calculus.showResult(Calculus.subtract(a,b));
+        case '*': Calculus.showResult(Calculus.multiply(a,b));
+        case '/': Calculus.showResult(Calculus.divide(a,b));
     }
+  }
 }
+
+
